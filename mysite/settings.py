5df6108b1e53870aef5c05c29624de2b11b8f07f
suganshakya <sugan.shakya@yahoo.com>
2016-08-09 10:42:15 +0545

    Edited allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8qj!uz))j83d(02u^w8$44xdeqm1m%lyz$3wx$1e@t#&-f@wtw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'sugan.pythonanywhere.com']
 
 
 # Application definition
